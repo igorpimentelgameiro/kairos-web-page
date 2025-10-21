45ebd48015d331cbc1d7e0c4ec9b8220d5b95e3c
Igor Pimentel Gameiro <contato.igor.pimentel@gmail.com>
2025-10-21 19:26:26 -0300

    correcoes

---
+++ src/main/javascript/src/main/javascript/componente/Navbar.types.ts
@@ -0,0 +1,9 @@
+export type PageKey = "HOME" | "QUEM_SOMOS" | "KASA" | "BENFEITOR" | "CONTATO";
+
+export const PAGES: Record<PageKey, string> = {
+    HOME: "Início",
+    QUEM_SOMOS: "Quem Somos",
+    KASA: "Retiro KASA",
+    BENFEITOR: "Seja Benfeitor",
+    CONTATO: "Contato",
+};
